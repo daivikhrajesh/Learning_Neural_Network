f8809f7f86a45a4f5480a1f8437187dffe59f260
daivikhrajesh <darkdevill7799@gmail.com>
2024-08-12 14:38:13 -0400

    -

--- Simple_nn_7/NN7.py
+++ Simple_nn_7/NN7.py
@@ -3,11 +3,15 @@ import math
 softmax_output = [0.7, 0.1, 0.2]
 target_output = [1, 0, 0]
 
-loss = -(math.log(softmax_output[0]) * target_output[0] +
+""" loss = -(math.log(softmax_output[0]) * target_output[0] +
          math.log(softmax_output[1]) * target_output[1] +
-         math.log(softmax_output[2]) * target_output[2])
+         math.log(softmax_output[2]) * target_output[2]) """
 
-print(loss)
+loss_ = 0
+for i,j in zip(softmax_output, target_output):
+    loss_+= -(math.log(i)*j)
 
-print(-math.log(0.7))
-print(-math.log(0.5))
+print(loss_)
+
+""" print(-math.log(0.7))
+print(-math.log(0.5)) """
